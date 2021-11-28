543d1fd9abf9d932b0f87bc1967c9808c785d7c5
"El Tecla" <"ch.roig@gmail.com">
2021-11-28 16:48:00 -0300

    Etapa 3: agregar direccion de pago.

---
+++ contracts/crowdfunding/Crowdfunding-3-2.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: GPL-3.0
+// ref: se tomo de base 1-Storage.sol (ej. de Remix)
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Crowdfunding3 {
+
+    // Etapa 3: Dirección de pago (ref:2_Owner.sol, ej Remix)
+    address private owner;
+    address payable private dirDePago;
+        
+    event OwnerSet(address indexed newOwner);// event for EVM logging
+        
+    constructor() {
+        owner = msg.sender; // 'msg.sender' is sender of current call, contract deployer for a constructor
+        dirDePago = payable(msg.sender);
+        emit OwnerSet( owner);
+    }
+    
+    // Etapa 1: Info del proyecto
+    string private proyecto = "Indefinido";
+    string private artista  = "Desconocido";       // nombre del artista o emprendedor
+    uint private aRecaudar  = 3000000000000000000; // monto TOTAL a recaudar (3 eth por default)
+
+    function setInfoProy(string memory nombre_proyecto, string memory nombre_artista, uint monto_a_recaudar) public {
+        proyecto  = nombre_proyecto;
+        artista   = nombre_artista;
+        aRecaudar = monto_a_recaudar;
+    }
+
+    function getInfoProy() public view returns (string memory, string memory, uint, address){
+        return (proyecto, artista, aRecaudar, dirDePago);
+    }
+
+    // Etapa 2: Donacion
+    address private origen; // direccion del ultimo donante
+    string private donante; // nombre del ultimo donante/aportante/mecenas
+    uint private monto;     // ultimo monto donado
+
+    function donar(string memory nombre_donante) public payable {
+        origen  = msg.sender;
+        donante = nombre_donante;
+        monto   = msg.value;
+    }
+
+    function getUltimaDonacion() public view returns (address, string memory, uint){
+        return (origen, donante, monto);
+    }
+
+    function balance() public view returns (uint){
+        return address(this).balance;
+    }
+
+    
+    
+}
