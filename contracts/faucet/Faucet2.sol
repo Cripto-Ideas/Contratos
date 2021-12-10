5d777cc2ed969587a0e4dd7eda78a656d91b77f3
"El Tecla" <"ch.roig@gmail.com">
2021-12-10 16:53:22 -0300

    version 3 de faucet: owner, contructor y destructo

--- contracts/faucet/Faucet2.sol
+++ contracts/faucet/Faucet2.sol
@@ -5,26 +5,13 @@ pragma solidity ^0.6.4;
 
 // Our first contract is a faucet!
 contract Faucet {
-    address payable owner;
-
-    // Contract constructor: set owner
-    constructor() public {
-        owner = msg.sender;
-    }
-
-    // Contract destructor
-    function destroy() public {
-        require(msg.sender == owner);
-        selfdestruct(owner);
-    }
-
     // Accept any incoming amount
     receive() external payable {}
-    
+
     // Give out ether to anyone who asks
     function withdraw(uint withdraw_amount) public {
         // Limit withdrawal amount
-        require(withdraw_amount <= 0.1 ether);
+        require(withdraw_amount <= 100000000000000000);
 
         // Send the amount to the address that requested it
         msg.sender.transfer(withdraw_amount);
